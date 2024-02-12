d1ac56ddc211481c4a44d8c9bbe2ada8f7ce0ca5
Your Name <ishanpoudel58@gmail.com>
2024-02-12 22:12:59 +0545

    darker commit

--- resources/views/admin/room/index.blade.php
+++ resources/views/admin/room/index.blade.php
@@ -44,8 +44,7 @@
                       <td>{{ $t->name }}</td>
                     @endif
                   @endforeach
-                  <td>
-                    
+                  <td>                    
                     <div class="dropdown">
                       <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
                         <i class="bx bx-dots-vertical-rounded"></i>
