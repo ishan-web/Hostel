38e7c8710459077ff31286bf3b9988ae17ab319d
Your Name <ishanpoudel58@gmail.com>
2024-02-20 23:51:46 +0545

    stu

---
+++ resources/views/layouts/studentmaster.blade.php
@@ -0,0 +1,44 @@
+
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+    <meta charset="utf-8">
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
+    />
+    <title> Hostel Dashboard</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('images/Dhuwani.png') }}">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    
+    @include('layouts.partials._adminstyle')
+    @yield('styles')
+</head>
+
+<body>
+
+        <div class="layout-wrapper layout-content-navbar">
+            <div class="layout-container">
+                @include('layouts.partials._usernavbar')
+                    <div class="layout-page">
+                        @include('layouts.partials._admintopbar')
+                                <!-- Content wrapper -->
+                                <div class="content-wrapper">
+                                    <!-- Content -->
+                                    @yield('usercontent')
+                                </div>
+                    </div>
+            </div>
+        </div>
+    
+
+@include('layouts.partials._adminscript')
+@yield('scripts')
+</body>
+
+</html>
